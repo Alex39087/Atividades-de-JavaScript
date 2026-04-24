dbc284cfbf890c893d0b82bc833fb0ad54149d3d
Alex39087 <engs-alexbecker@camporeal.edu.br>
2026-04-23 22:13:22 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById('meuBotao');
+  const div = document.getElementById('meuDiv');
+
+  botao.addEventListener('click', function() {
+    p.style.backgroundColor = 'blue'; 
+    p.style.color = 'black';  
+
+  });
---
+++ atividade2.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById('Botao');
+  const div = document.getElementById('meuDiv');
+
+  botao.addEventListener('click', function() {
+    div.style.backgroundColor = 'blue'; 
+    div.style.color = 'black';  
+
+  });
