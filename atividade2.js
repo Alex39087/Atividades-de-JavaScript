260272855043251a80f8152af8c4b46c2da14939
Alex39087 <engs-alexbecker@camporeal.edu.br>
2026-04-24 16:02:01 -0300

    Add files via upload

--- atividade2.js
+++ atividade2.js
@@ -1,8 +1,25 @@
 const botao = document.getElementById('Botao');
-  const div = document.getElementById('meuDiv');
+const fundo = document.body;
 
   botao.addEventListener('click', function() {
-    div.style.backgroundColor = 'blue'; 
-    div.style.color = 'black';  
+    if(fundo.style.backgroundColor === 'lightblue') {
+        fundo.style.backgroundColor = 'lightcoral';
+    } else {
+      fundo.style.backgroundColor = 'lightblue';
+    }
+  })
+
+const botao2 = document.getElementById('Botao2');
+const h2 = document.querySelector('h2');
+
+  botao2.addEventListener('click', function() {
+    if(h2.style.backgroundColor === 'lightblue') {
+        h2.style.backgroundColor = 'lightcoral';
+    } else {
+      h2.style.backgroundColor = 'lightblue';
+    }
+
+
+
 
   });
